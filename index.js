2dc2b10b54b511d0782d8b85db13a3c0f53dfc03
Tohir Arapjonov <toxirarapjonov@gmail.com>
2024-05-26 20:15:54 +0500

    dsfsdf

---
+++ index.js
@@ -0,0 +1,7 @@
+let birinchison = Number(prompt("1 - soni kiriting:"));
+let ikkinchison = Number(prompt("2 - soni kiriting:"));
+
+alert(`${birinchison} + ${ikkinchison} = ${birinchison + ikkinchison}`);
+alert(`${birinchison} - ${ikkinchison} = ${birinchison + ikkinchison}`);
+alert(`${birinchison} - ${ikkinchison} = ${birinchison + ikkinchison}`);
+alert(`${birinchison} - ${ikkinchison} = ${birinchison + ikkinchison}`);        
